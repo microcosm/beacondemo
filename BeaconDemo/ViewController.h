27b758d4b2928b736db4ee0791e4ba4e7f69ea41
Dexter Teng <dexterteng89@gmail.com>
2013-12-23 11:13:59 -0600

    Initial Commit

---
+++ BeaconDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BeaconDemo
+//
+//  Created by dteng on 12/23/13.
+//  Copyright (c) 2013 dteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BeaconDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BeaconDemo
+//
+//  Created by dteng on 12/23/13.
+//  Copyright (c) 2013 dteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
