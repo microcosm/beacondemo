30ce8c6442c406b10d8e7c21c4b2a44e6043cd77
Dexter Teng <dexterteng89@gmail.com>
2013-12-24 12:13:46 -0600

    Have the application go to first page when launched from background; Added a WishListViewController

---
+++ BeaconDemo/WishListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WishListViewController.h
+//  BeaconDemo
+//
+//  Created by dteng on 12/24/13.
+//  Copyright (c) 2013 dteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WishListViewController : UIViewController
+
+@end
---
+++ BeaconDemo/WishListViewController.m
@@ -0,0 +1,38 @@
+//
+//  WishListViewController.m
+//  BeaconDemo
+//
+//  Created by dteng on 12/24/13.
+//  Copyright (c) 2013 dteng. All rights reserved.
+//
+
+#import "WishListViewController.h"
+
+@interface WishListViewController ()
+
+@end
+
+@implementation WishListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
