c26889d835f76b3739c2b20776aa74bca6602245
dlockman <lockman.daniel@gmail.com>
2013-12-27 15:24:02 -0500

    Set up basic location tracking, identifier for closest beacon.

--- BeaconDemo/MapViewController.m
+++ BeaconDemo/MapViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "MapViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface MapViewController ()
+@interface MapViewController () <CLLocationManagerDelegate>
+
+    @property (nonatomic) CLBeaconRegion *beaconRegion;
+    @property (nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -17,8 +21,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    [self setupBeaconTracking];
 }
+
+- (void)setupBeaconTracking {
+	self.locationManager = [[CLLocationManager alloc] init];
+	self.locationManager.delegate = self;
+	
+	NSUUID *uuid = [[NSUUID alloc] initWithUUIDString: @"B9407F30-F5F8-466E-AFF9-25556B57FE6D"];
+	self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID: uuid identifier: @"thoughtWorksNRF"];
+    [self.locationManager startMonitoringForRegion: self.beaconRegion];
+    [self.locationManager startRangingBeaconsInRegion: self.beaconRegion];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
+    
+    NSMutableArray *distances = [[NSMutableArray alloc] initWithCapacity:3];
+    NSMutableArray *identifiers = [[NSMutableArray alloc] initWithCapacity:3];
+    NSString *identifier = [[NSString alloc] init];
+    CLBeacon *beacon = [beacons objectAtIndex:0];
+    
+    identifier = [NSString stringWithFormat: @"%@%@%@", beacon.proximityUUID.UUIDString, beacon.major, beacon.minor];
+    [UpdateMap identifier]
+}
+
 
 - (void)didReceiveMemoryWarning
 {
