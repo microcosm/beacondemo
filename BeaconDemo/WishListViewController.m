ee2ad1cfabea91a00e3435c7b2253d4214b0c5b7
Andrew McWilliams <andy@jahya.net>
2014-01-09 14:15:58 -0500

    Adds new styling on first page

--- BeaconDemo/WishListViewController.m
+++ BeaconDemo/WishListViewController.m
@@ -9,6 +9,9 @@
 #import "WishListViewController.h"
 
 @interface WishListViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextView *text;
+@property (weak, nonatomic) IBOutlet UIView *background;
 
 @end
 
@@ -18,7 +21,17 @@
 {
     [super viewDidLoad];
     
-
+    self.background.backgroundColor = [self colorWithHexString:@"efeff4"];
+    
+    self.label.font = [UIFont fontWithName:@"OpenSans-ExtraBold" size:25];
+     self.label.textColor = [self colorWithHexString:@"2c3e50"];
+     self.label.backgroundColor = [UIColor colorWithHue:0 saturation:0 brightness:0 alpha:0];
+     self.label.textAlignment = NSTextAlignmentCenter;
+    
+    self.text.font = [UIFont fontWithName:@"OpenSans-Light" size:15];
+     self.text.textColor = [self colorWithHexString:@"2c3e50"];
+     self.text.backgroundColor = [UIColor colorWithHue:0 saturation:0 brightness:0 alpha:0];
+     self.text.textAlignment = NSTextAlignmentCenter;
 
 }
 
@@ -27,6 +40,42 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+}
+
+-(UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
 }
 
 @end
