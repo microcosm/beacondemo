523728f520dacfd156d46f2c1b1877293ad7c2ee
Andrew McWilliams <andy@jahya.net>
2014-01-09 20:53:42 -0500

    Adds background color to wait a minute page

---
+++ BeaconDemo/WaitAMomentViewController.h
@@ -0,0 +1,13 @@
+//
+//  WaitAMomentViewController.h
+//  nrfbeacondemo
+//
+//  Created by Andy on 1/9/14.
+//  Copyright (c) 2014 dteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaitAMomentViewController : UIViewController
+
+@end
