f86ad2e547b0b3e3db16f4ebd15edd359b358868
Andrew McWilliams <andy@jahya.net>
2014-01-09 19:14:31 -0500

    Styles second page

---
+++ BeaconDemo/WishListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WishListViewController.h
+//  nrfbeacondemo
+//
+//  Created by Andy on 1/9/14.
+//  Copyright (c) 2014 dteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WishListViewController : UIViewController
+
+@end
---
+++ BeaconDemo/WishListViewController.m
@@ -0,0 +1,75 @@
+//
+//  WishListViewController.m
+//  nrfbeacondemo
+//
+//  Created by Andy on 1/9/14.
+//  Copyright (c) 2014 dteng. All rights reserved.
+//
+
+#import "WishListViewController.h"
+
+@interface WishListViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *background;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextView *text;
+
+@end
+
+@implementation WishListViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.background.backgroundColor = [self colorWithHexString:@"efeff4"];
+    
+    self.label.font = [UIFont fontWithName:@"OpenSans-ExtraBold" size:25];
+    self.label.textColor = [self colorWithHexString:@"2c3e50"];
+    self.label.backgroundColor = [UIColor colorWithHue:0 saturation:0 brightness:0 alpha:0];
+    self.label.textAlignment = NSTextAlignmentCenter;
+    
+    self.text.font = [UIFont fontWithName:@"OpenSans-Light" size:15];
+    self.text.textColor = [self colorWithHexString:@"2c3e50"];
+    self.text.backgroundColor = [UIColor colorWithHue:0 saturation:0 brightness:0 alpha:0];
+    self.text.textAlignment = NSTextAlignmentCenter;
+    
+}
+
+
+-(UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
+}
+@end
