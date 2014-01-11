5a703c41c32f1379ed5433ab50a335fca4cbc286
dlockman <lockman.daniel@gmail.com>
2014-01-11 14:49:34 -0500

    Moved points and added additional ones

--- BeaconDemo/Model/BeaconLayoutManager.m
+++ BeaconDemo/Model/BeaconLayoutManager.m
@@ -61,12 +61,24 @@ static const int OFFSET_X = 65;
                                        //Top left
                                        @"B9407F30-F5F8-466E-AFF9-25556B57FE6D:53626:34111":
                                            [ NSValue valueWithCGPoint: CGPointMake(OFFSET_X + 30, 80)],
+//                                       //Top Middle
+//                                       @"B9407F30-F5F8-466E-AFF9-25556B57FE6D:32002:57685":
+//                                           [ NSValue valueWithCGPoint: CGPointMake(OFFSET_X + 70, 10)],
                                        //Top right
                                        @"B9407F30-F5F8-466E-AFF9-25556B57FE6D:61612:59156":
                                            [ NSValue valueWithCGPoint: CGPointMake(OFFSET_X + 110, 90)],
+                                       //Middle Top Right
+                                       @"B9407F30-F5F8-466E-AFF9-25556B57FE6D:59820:47378":
+                                           [ NSValue valueWithCGPoint: CGPointMake(OFFSET_X + 110, 147)],
+                                       //Middle Bottom Right
+                                       @"B9407F30-F5F8-466E-AFF9-25556B57FE6D:13272:41609":
+                                           [ NSValue valueWithCGPoint: CGPointMake(OFFSET_X + 110, 204)],
                                        //Bottom left
                                        @"B9407F30-F5F8-466E-AFF9-25556B57FE6D:55284:5223":
                                            [ NSValue valueWithCGPoint: CGPointMake(OFFSET_X + 30, 260)],
+//                                       //Bottom Middle
+//                                       @"B9407F30-F5F8-466E-AFF9-25556B57FE6D:43188:62110":
+//                                           [ NSValue valueWithCGPoint: CGPointMake(OFFSET_X + 70, 320)],
                                        //Bottom right
                                        @"B9407F30-F5F8-466E-AFF9-25556B57FE6D:25061:32695":
                                            [ NSValue valueWithCGPoint: CGPointMake(OFFSET_X + 110, 260)]
